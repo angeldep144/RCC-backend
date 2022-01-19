acdeed71d809ae8fbf2d8d863460562cd408bdf3
bradywestveer-revature <93550118+bradywestveer-revature@users.noreply.github.com>
2022-01-18 18:42:26 -0600

    removed unused table name property in Transaction model

--- src/main/java/com/revature/project3backend/models/Transaction.java
+++ src/main/java/com/revature/project3backend/models/Transaction.java
@@ -11,7 +11,7 @@ import java.util.List;
 @AllArgsConstructor
 @Data
 @Entity
-@Table (name = "transaction")
+@Table
 public class Transaction {
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
