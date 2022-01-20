6247469465d28852f7911867934e46fc014649d0
bradywestveer-revature <93550118+bradywestveer-revature@users.noreply.github.com>
2022-01-20 10:26:27 -0600

    added ProductRepo javadocs and integration tests

--- src/main/java/com/revature/project3backend/repositories/ProductRepo.java
+++ src/main/java/com/revature/project3backend/repositories/ProductRepo.java
@@ -6,5 +6,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepo extends JpaRepository <Product, Integer> {
+	/**
+	 * Returns a page of products based on a name and description. Case is ignored and products must only contain the passed name or description
+	 * @param name The string that the product's name must contain to be included in the result (if not found by description)
+	 * @param description The string that the product's description must contain to be included in the result (if not found by name)
+	 * @param pageable The Pageable object to use to get the page of products
+	 * @return Returns a Page containing the products that were found
+	 */
 	Page <Product> findByNameIgnoreCaseContainingOrDescriptionIgnoreCaseContaining (String name, String description, Pageable pageable);
 }
