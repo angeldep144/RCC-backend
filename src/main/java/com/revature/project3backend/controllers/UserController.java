a25fd16dbb70610cd131688e651e3ef469f38b1a
Richard <rmace1@yahoo.com>
2022-01-20 11:53:52 -0500

    tests and login stuff

--- src/main/java/com/revature/project3backend/controllers/UserController.java
+++ src/main/java/com/revature/project3backend/controllers/UserController.java
@@ -1,8 +1,13 @@
 package com.revature.project3backend.controllers;
 
+import com.revature.project3backend.exceptions.InvalidCredentialsException;
 import com.revature.project3backend.exceptions.InvalidValueException;
 import com.revature.project3backend.jsonmodels.JsonResponse;
 import com.revature.project3backend.models.User;
+import com.revature.project3backend.services.UserService;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,10 +15,18 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping ("user")
 @CrossOrigin (origins = "http://localhost:4200", allowCredentials = "true")
 public class UserController {
+	Logger log = Logger.getLogger(UserController.class);
+
+	UserService userService;
+
+	@Autowired
+	public UserController(UserService userService){this.userService = userService;}
+
 	@PostMapping
 	public ResponseEntity <JsonResponse> createUser (@RequestBody User body) throws InvalidValueException {
 		//todo
-		
+
 		return ResponseEntity.ok (new JsonResponse ("Created user", true, null, "/login"));
 	}
+
 }
---
+++ src/test/java/com/revature/project3backend/controllers/SessionControllerTest.java
@@ -0,0 +1,16 @@
+package com.revature.project3backend.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SessionControllerTest {
+
+    @Test
+    void createSession() {
+    }
+
+    @Test
+    void deleteSession() {
+    }
+}
