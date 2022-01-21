d6479b198877cdad10e097a561519c12931234e6
David-Bratkov <bratkovdavid09@gmail.com>
2022-01-21 08:08:22 -0800

    added java docs for the methods related to register

--- src/main/java/com/revature/project3backend/repositories/UserRepo.java
+++ src/main/java/com/revature/project3backend/repositories/UserRepo.java
@@ -4,7 +4,17 @@ import com.revature.project3backend.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepo extends JpaRepository <User, Integer> {
+	/**
+	 *
+	 * @param username a string that will search the database in the Users table for a matching value in the username column
+	 * @return The user found in the database and if none is found it will return null
+	 */
 	User findByUsername (String username);
-	
+
+	/**
+	 *
+	 * @param email a string that will search the database in the Users table for a matching value in the emails column
+	 * @return The user found in the database and if none is found it will return null
+	 */
 	User findByEmail (String email);
 }
