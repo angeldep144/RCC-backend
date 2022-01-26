8dd924209111a83da282630724dc225193e3ad20
Richard <rmace1@yahoo.com>
2022-01-26 18:40:53 -0500

    edited project service

--- src/test/java/com/revature/project3backend/services/ProductServiceTest.java
+++ src/test/java/com/revature/project3backend/services/ProductServiceTest.java
@@ -4,6 +4,7 @@ import com.revature.project3backend.exceptions.InvalidValueException;
 import com.revature.project3backend.models.Product;
 import com.revature.project3backend.repositories.ProductRepo;
 import com.revature.project3backend.utils.FileUtil;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -29,8 +30,15 @@ class ProductServiceTest {
 	
 	List <Product> products = new ArrayList <> ();
 
+	@BeforeAll
+	static void beforeAll(){
+		Mockito.mockStatic(FileUtil.class);
+
+	}
+
 	@BeforeEach
 	void beforeEach(){
+
 		mf = new MultipartFile() {
 			@Override
 			public String getName() {
@@ -149,7 +157,6 @@ class ProductServiceTest {
 	void createProductWithValidInputAndFile() throws InvalidValueException {
 		Product product = new Product (1, "name", "description", 10F, null, null, null);
 
-		Mockito.mockStatic(FileUtil.class);
 		Mockito.when(FileUtil.uploadToS3(product, this.mf)).thenReturn("https://s3-alpha.figma.com/hub/file/948140848/1f4d8ea7-e9d9-48b7-b70c-819482fb10fb-cover.png");
 		Mockito.when(productRepo.save(product)).thenReturn(product);
 
@@ -222,7 +229,6 @@ class ProductServiceTest {
 	void updateProductWithFilePositive() throws InvalidValueException {
 		Product product = new Product (1, "name", "description", 10F, null, null, null);
 
-		Mockito.mockStatic(FileUtil.class);
 		Mockito.when(FileUtil.uploadToS3(product, this.mf)).thenReturn("https://s3-alpha.figma.com/hub/file/948140848/1f4d8ea7-e9d9-48b7-b70c-819482fb10fb-cover.png");
 		Mockito.when(productRepo.save(product)).thenReturn(product);
 
@@ -236,7 +242,6 @@ class ProductServiceTest {
 	void updateProductWithoutFilePositive() throws InvalidValueException {
 		Product product = new Product (1, "name", "description", 10F, null, null, null);
 
-		Mockito.mockStatic(FileUtil.class);
 		Mockito.when(FileUtil.uploadToS3(product, null)).thenReturn("https://s3-alpha.figma.com/hub/file/948140848/1f4d8ea7-e9d9-48b7-b70c-819482fb10fb-cover.png");
 		Mockito.when(productRepo.save(product)).thenReturn(product);
 
@@ -249,7 +254,6 @@ class ProductServiceTest {
 	void updateProductWithoutNameNegative() throws InvalidValueException {
 		Product product = new Product (1, null, "description", 10F, null, null, null);
 
-		Mockito.mockStatic(FileUtil.class);
 		Mockito.when(FileUtil.uploadToS3(product, null)).thenReturn("https://s3-alpha.figma.com/hub/file/948140848/1f4d8ea7-e9d9-48b7-b70c-819482fb10fb-cover.png");
 		Mockito.when(productRepo.save(product)).thenReturn(product);
 
@@ -270,7 +274,6 @@ class ProductServiceTest {
 	void updateProductWithoutDescriptionNegative() throws InvalidValueException {
 		Product product = new Product (1, "name", null, 10F, null, null, null);
 
-		Mockito.mockStatic(FileUtil.class);
 		Mockito.when(FileUtil.uploadToS3(product, null)).thenReturn("https://s3-alpha.figma.com/hub/file/948140848/1f4d8ea7-e9d9-48b7-b70c-819482fb10fb-cover.png");
 		Mockito.when(productRepo.save(product)).thenReturn(product);
 
