49e8579b8d4f15e68761c21cec84ef8efbc7b983
Alfred Garcia <garcia.alfredm@gmail.com>
2022-01-26 12:37:46 -0500

    removed unused commented code

--- src/test/java/com/revature/project3backend/controllers/TransactionControllerIT.java
+++ src/test/java/com/revature/project3backend/controllers/TransactionControllerIT.java
@@ -113,45 +113,4 @@ class TransactionControllerIT {
                 .andExpect (MockMvcResultMatchers.status ().isBadRequest ())
                 .andExpect (MockMvcResultMatchers.content ().json (json.writeValueAsString (new JsonResponse (new InvalidValueException ("Invalid transaction id")))));
     }
-	
-	/*
-    @Test
-    public void createTransactionPositive() throws Exception {
-
-        User user = new User ("first", "last", "email", "username", "password");
-
-        Product product = new Product(1, "roomba", "description", 12.88f, "https://i.pcmag.com/imagery/reviews/01hmxcWyN13h1LfMglNxHGC-1.fit_scale.size_1028x578.v1589573902.jpg", 12.00f, 10);
-
-        Product product0 = new Product(1, "roomba", "description", 12.88f, "https://i.pcmag.com/imagery/reviews/01hmxcWyN13h1LfMglNxHGC-1.fit_scale.size_1028x578.v1589573902.jpg", 12.00f, 10);
-        List<CartItem> items = new ArrayList<>();
-
-        items.add(new CartItem(user,product,1));
-        items.add(new CartItem(user,product0,1));
-
-
-        Transaction transaction = new Transaction(user);
-        Transaction expected = new Transaction(1,user,items.toString(),25.76f);
-
-        Mockito.when(this.transactionService.createTransaction(transaction,items)).thenReturn(expected);
-
-        HttpSession httpSession = new MockHttpSession();
-        httpSession.setAttribute("user",user);
-
-        CreateSessionBody createSessionBody = new CreateSessionBody ();
-
-        createSessionBody.setIdentifier ("username");
-        createSessionBody.setPassword ("password");
-
-
-        mvc.perform(MockMvcRequestBuilders.post("/transaction")
-                .contentType(MediaType.APPLICATION_JSON)
-                .session (new MockHttpSession ())
-                .content(json.writeValueAsString(createSessionBody)))
-                .andExpect (MockMvcResultMatchers.status ().isOk ())
-                .andExpect (MockMvcResultMatchers.content ().json (json.writeValueAsString (new JsonResponse ("Created transaction", true, transactionService.createTransaction (new Transaction (user), items)))));
-
-
-
-    }
-	*/
 }
