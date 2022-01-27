c6e5f8e2dcd258176cdb15959c523b65fa3afa8c
bradywestveer-revature <93550118+bradywestveer-revature@users.noreply.github.com>
2022-01-27 15:51:53 -0600

    fixed error with UserRoleRepoIT caused by duplicate user email in import.sql

--- src/test/resources/import.sql
+++ src/test/resources/import.sql
@@ -1,9 +1,5 @@
-INSERT INTO USERS (FIRST_NAME, LAST_NAME, EMAIL, USERNAME, PASSWORD)values('Test', 'User', 'testuser@email.com', 'TestUser', 'pass123');
-
 INSERT INTO user_roles VALUES (DEFAULT, 'USER');
 
 INSERT INTO user_roles VALUES (DEFAULT, 'ADMIN');
 
 INSERT INTO USERS (FIRST_NAME, LAST_NAME, EMAIL, USERNAME, PASSWORD, ROLE_ID)values('Test', 'User', 'testuser@email.com', 'TestUser', 'pass123', 2);
-
-
