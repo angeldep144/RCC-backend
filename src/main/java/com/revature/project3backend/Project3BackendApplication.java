134631d7f2f5c148721e963e4f8146db281e433a
bradywestveer-revature <93550118+bradywestveer-revature@users.noreply.github.com>
2022-01-27 10:51:15 -0600

    added javadoc

--- src/main/java/com/revature/project3backend/Project3BackendApplication.java
+++ src/main/java/com/revature/project3backend/Project3BackendApplication.java
@@ -3,6 +3,9 @@ package com.revature.project3backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The main class of the application
+ */
 @SpringBootApplication
 public class Project3BackendApplication {
 	/**
